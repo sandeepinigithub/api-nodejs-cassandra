63079eb4997f68369d682e947709e3426429c429
Sandeep Kumar Shukla <sandeepini.2012@gmail.com>
2023-01-24 11:52:31 +0530

    initial setup

---
+++ config/db.config.js
@@ -0,0 +1,9 @@
+const dbConfig = {
+    user: '',
+    host: '',
+    database: '',
+    password: '',
+    port: 1111,
+};
+
+module.exports = dbConfig;
---
+++ controllers/user.controller.js
@@ -0,0 +1,6 @@
+const userQuery = require('../models/user.model');
+const dbConfig = require('../config/db.config');
+
+module.exports.getAllUser = async (req, res) => {
+    res.send("Hello!!")
+}
